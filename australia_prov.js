408b14eb93535d93518820e64d3fcc59a9e2daf0
kenyanbapu <dhvaghela@gmail.com>
2018-02-12 14:00:46 -0600

    All Files

---
+++ australia_prov.js
@@ -0,0 +1,10 @@
+var australian_prov = {
+    "ACT": "Australian Capital Territory",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "QLD": "Queensland",
+    "SA": "South Australia",
+    "TAS": "Tasmania",
+    "VIC": "Victoria",
+    "WA": "Western Australia"
+}
